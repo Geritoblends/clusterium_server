742d7f7faad45e528ab85505966a167fa8ed88b4
Gero <geronimosotelolopez@gmail.com>
2025-08-02 19:29:25 -0600

    data pruning handling in the schema, and lib.rs rework with the new schema

--- crates/stack-ledger/lib.rs
+++ crates/stack-ledger/lib.rs
@@ -1,28 +1,50 @@
 use sqlx::{PgPool, Transaction, Postgres};
 use thiserror::{Error as ThisError};
 use std::hash::Hasher;
-use twox_hash::XxHash128;
+use twox_hash::XxHash3_128;
 
 #[derive(Debug, ThisError)]
 pub enum Error {
+
     #[error("Error from Sqlx: {0}")]
     Sqlx(#[from] sqlx::Error),
 
     #[error("Item type mismatch for stack '{stack_uuid}': expected {expected}, got {actual}")]
     ItemTypeMismatch {
-        stack_uuid: u64,
+        account_id: &str,
+        stack_uuid: u128,
         expected: u32,
         actual: u32,
     },
+
+    #[error("Not enough balance for stack '{stack_uuid}': requested {qty}, had {balance}")]
+    NotEnoughBalance {
+        account_id: &str,
+        stack_uuid: u128,
+        qty: u32,
+        balance: u32,
+    },
+
 }
 
 fn compute_hash(x: i128, y: i128, z: i128, a: u32) -> u128 {
-    let mut hasher = XxHash128::with_seed(0);  
-    hasher.write_i128_le(x);
-    hasher.write_i128_le(y);
-    hasher.write_i128_le(z);
-    hasher.write_u32_le(a);
-    hasher.finish()  
+    let mut bytes = [0u8; 52];  
+    
+    bytes[0..16].copy_from_slice(&x.to_le_bytes());
+    bytes[16..32].copy_from_slice(&y.to_le_bytes());
+    bytes[32..48].copy_from_slice(&z.to_le_bytes());
+    bytes[48..52].copy_from_slice(&a.to_le_bytes());
+    
+    XxHash3_128::oneshot(&bytes)
+}
+
+fn compute_hash_key(account_id: &str, stack_uuid: u128) -> u128 {
+    let account_bytes = account_id.as_bytes();
+    let uuid_bytes = stack_uuid.to_le_bytes();
+    let mut bytes = Vec::with_capacity(account_bytes.len() + 16);
+    bytes.extend_from_slice(account_bytes);
+    bytes.extend_from_slice(&uuid_bytes);
+    XxHash3_128::oneshot(&bytes)
 }
 
 struct StackLedger {
@@ -44,8 +66,9 @@ impl StackLedger {
         }
     }
 
-    pub async fn create(tx: &mut Transaction<'_, Postgres>, stack_uuid: u64, item_type: u32, qty: u32, account_id: &str) -> Result<(), Error> {
+    async fn create(tx: &mut Transaction<'_, Postgres>, stack_uuid: u128, item_type: u32, qty: u32, account_id: &str) -> Result<(), Error> {
 
+        let latest_key = compute_hash_key(account_id, stack_uuid);
         // Garantiza que solo un jugador pueda obtener el drop
         sqlx::query!(
             r#"INSERT INTO consumed (stack_uuid)
@@ -55,39 +78,56 @@ impl StackLedger {
             .execute(&mut **tx)
             .await?;
 
-
-        // Inserción al ledger que solo sucede si se cumple UNIQUE(stack_uuid, account_id,
-        // sequence_number), lo cual elimina dupes de race conditions de raìz.
-        sqlx::query!(
-            r#"INSERT INTO ledger (stack_uuid, account_id, item_type, qty, balance, sequence_number)
-            VALUES ($1, $2, $3, $4, $5, $6);
+        let ledger_entry = sqlx::query!(
+            r#"INSERT INTO ledger (account_id, stack_uuid, sequence_number, qty, balance, item_type)
+            VALUES ($1, $2, $3, $4, $5, $6)
+            RETURNING key;
             "#,
-            stack_uuid,
             account_id,
-            item_type,
+            stack_uuid,
+            0,
             qty,
             qty,
-            0)
-            .execute(&mut **tx)
+            item_type)
+            .fetch_one(&mut **tx)
             .await?;
 
         // Sirve para posteriores inserciones al ledger
         sqlx::query!(
-            r#"INSERT INTO latest (sequence_number, balance, item_type, account_id, stack_uuid)
+            r#"INSERT INTO latest (key, account_id, stack_uuid, sequence_number, balance, item_type)
             VALUES ($1, $2, $3, $4, $5, $6);
             "#,
+            latest_key,
+            account_id,
+            stack_uuid,
             0,
             qty,
-            item_type,
-            account_id,
-            stack_uuid)
+            item_type)
+            .execute(&mut **tx)
+            .await?;
+
+       sqlx::query!(
+           r#"INSERT INTO stacks (stack_uuid, latest_keys, ledger_entries)
+           VALUES ($1, $2, $3);"#,
+           stack_uuid.to_le_bytes().as_slice(),
+           &[latest_key],
+           &[ledger_entry.key])
+           .execute(&mut **tx)
+           .await?;
+
+        sqlx::query!(
+            r#"UPDATE inventories 
+            SET stack_uuids = array_append(stack_uuids, $1)
+            WHERE account_id = $2;"#,
+            stack_uuid.to_le_bytes().as_slice(), // Single element array
+            account_id)
             .execute(&mut **tx)
             .await?;
 
         Ok(())
     }
 
-    pub async fn destroy(tx: &mut Transaction<'_, Postgres>, stack_uuid: u64, expected_item_type: u32, account_id: &str, qty: u32) -> Result<(), Error> {
+    pub async fn destroy(tx: &mut Transaction<'_, Postgres>, stack_uuid: u128, expected_item_type: u32, account_id: &str, qty: u32) -> Result<(), Error> {
         /// Method to destroy a quantity from an existing stack
         /// for performance reasons, stack_uuid might be provided by the client, that is why we need to make sure the
         /// expected_item_type is determined server-side, this way we can compare the
@@ -95,16 +135,14 @@ impl StackLedger {
         /// the possibility of the client lying about it being owner of a stack, but that is solved
         /// when querying the "latest" table for the account_id+stack_uuid key.
 
-        let qty: i32 = -qty;
-
         // Real current values table. This select also ensures the account_id really has said stack
         let latest = sqlx::query!(r#"
-        SELECT sequence_number, balance, item_type
+        SELECT key, sequence_number, balance, item_type
         FROM latest
-        WHERE account_id = $1 AND stack_uuid = $2;
+        WHERE key = $1;
         "#,
-        account_id,
-        stack_uuid)
+        compute_hash_key(account_id,
+        stack_uuid).to_le_bytes().as_slice())
             .fetch_one(&mut **tx)
             .await?;
         
@@ -112,9 +150,20 @@ impl StackLedger {
         if expected_item_type != latest.item_type {
             tx.rollback().await?;
             return Err(Error::ItemTypeMismatch {
-                stack_uuid: stack_uuid.to_string(),
+                account_id,
+                stack_uuid,
                 expected: expected_item_type,
                 actual: latest.item_type,
+            });
+        }
+
+        if qty > latest.balance {
+            tx.rollback().await?;
+            return Err(Error::NotEnoughBalance {
+                account_id,
+                stack_uuid,
+                quantity: qty,
+                balance: latest.balance
             });
         }
 
@@ -141,17 +190,40 @@ impl StackLedger {
         account_id,
         stack_uuid)
             .execute(&mut **tx)
-            .await?
-            
+            .await?;
+
+        let empty_stack: bool = latest.balance == qty;           
+
+        if empty_stack {
+            sqlx::query!(r#"
+            UPDATE inventories
+            SET latest_keys = array_remove(latest_keys, $1)
+            WHERE account_id = $2;"#,
+            latest.key,
+            account_id)
+                .execute(&mut **tx)
+                .await?;
+
+            sqlx::query!(r#"
+            UPDATE stacks
+            SET latest_keys = array_remove(latest_keys, $1)
+            WHERE stack_uuid = $2;"#,
+            latest.key,
+            stack_uuid)
+                .execute(&mut **tx)
+                .await?;
+
+        }
+
             Ok(())
     }
 
-    pub async fn split(&self, tx: &mut Transaction<'_, Postgres>, stack_uuid: u64, expected_item_type: u32, sender_id: &str, recipient_id: &str, qty: u32) -> Result<(), Error> {
+    pub async fn split(&self, tx: &mut Transaction<'_, Postgres>, stack_uuid: u128, expected_item_type: u32, sender_id: &str, recipient_id: &str, qty: u32) -> Result<(), Error> {
 
         self.destroy(tx, stack_uuid, expected_item_type, sender_id, qty).await?;
 
         let result = sqlx::query!(
-            r#"SELECT  sequence_number, balance, item_type
+            r#"SELECT  key, sequence_number, balance, item_type
             FROM latest
             WHERE account_id = $1 AND stack_uuid = $2;
             "#,
@@ -187,49 +259,129 @@ impl StackLedger {
                     .execute(&mut **tx)
                     .await?;
 
-                },
-
-            Err(sqlx::Error::RowNotFound) => {
+                if latest.balance == 0 {
 
                     sqlx::query!(
-                        r#"INSERT INTO ledger (stack_uuid, account_id, item_type, qty, current_balance, sequence_number)
-                        VALUES ($1, $2, $3, $4, $5, $6);
-                        "#,
-                        stack_uuid,
-                        recipient_id,
-                        expected_item_type,
-                        qty,
-                        qty,
-                        0)
+                        r#"UPDATE inventories
+                        SET latest_keys = array_append(latest_keys, $1)
+                        WHERE account_id = $2;"#,
+                        latest.key,
+                        recipient_id)
                         .execute(&mut **tx)
                         .await?;
 
                     sqlx::query!(
-                        r#"INSERT INTO latest (sequence_number, balance, item_type, account_id, stack_uuid)
-                        VALUES ($1, $2, $3, $4, $5)
-                        "#,
-                        0,
-                        qty,
-                        expected_item_type,
-                        recipient_id,
+                        r#"UPDATE stacks
+                        SET latest_keys = array_append(latest_keys $1)
+                        WHERE stack_uuid = $2;"#,
+                        latest.key,
                         stack_uuid)
                         .execute(&mut **tx)
                         .await?;
 
-                    },
+                }
 
-                Err(e) => {
-                    tx.rollback().await?;
-                    return Err(e);
-                },
+            },
+
+            Err(sqlx::Error::RowNotFound) => {
+
+                sqlx::query!(
+                    r#"INSERT INTO ledger (stack_uuid, account_id, item_type, qty, current_balance, sequence_number)
+                    VALUES ($1, $2, $3, $4, $5, $6);
+                    "#,
+                    stack_uuid,
+                    recipient_id,
+                    expected_item_type,
+                    qty,
+                    qty,
+                    0)
+                    .execute(&mut **tx)
+                    .await?;
+
+                let latest_key = compute_hash_key(recipient_id, stack_uuid);
+                let latest = sqlx::query!(
+                    r#"INSERT INTO latest (key, account_id, stack_uuid, sequence_number, balance, item_type)
+                    VALUES ($1, $2, $3, $4, $5, $6);
+                    "#,
+                    latest_key.to_le_bytes().as_slice(),
+                    recipient_id,
+                    stack_uuid.to_le_bytes().as_slice(),
+                    0,
+                    qty,
+                    expected_item_type)
+                    .execute(&mut **tx)
+                    .await?;
+
+                sqlx::query!(
+                    r#"UPDATE inventories
+                    SET latest_keys = array_append(latest_keys, $1)
+                    WHERE account_id = $2;"#,
+                    latest_key.to_le_bytes().as_slice(),
+                    recipient_id)
+                    .execute(&mut **tx)
+                    .await?;
+
+                sqlx::query!(
+                    r#"UPDATE stacks
+                    SET latest_keys = array_append(latest_keys, $1)
+                    WHERE stack_uuid = $2;"#,
+                    latest_key.to_le_bytes().as_slice(),
+                    stack_uuid.to_le_bytes().as_slice())
+                    .execute(&mut **tx)
+                    .await?;
+
+
+            },
+
+            Err(e) => {
+                tx.rollback().await?;
+                return Err(e);
+            },
 
         };
 
         Ok(())
     }
 
-    pub async fn create_from_xyza(&self, x: i128, y: i128, z: i128, a: u32, item_type: u32, qty: u32, account_id: &str) -> Result<u64, Error> {
-        let stack_uuid: u64 = compute_hash(x, y, z, a); 
+    pub async fn create_from_xyza(&self, tx: &mut Transaction<'_, Postgres>, x: i128, y: i128, z: i128, a: u32, item_type: u32, qty: u32, account_id: &str) -> Result<u128, Error> {
+        let stack_uuid: u128 = compute_hash(x, y, z, a); 
+
+        self.create(tx, stack_uuid, item_type, qty, account_id).await?;
+        Ok(stack_uuid)
+    }
+}
+
+struct Stack {
+    uuid: u128,
+    balance: u32,
+    item_type: u32,
+}
+
+impl Stack {
+
+    pub fn get_uuid(&self) -> u128 {
+        self.uuid
+    }
+
+    pub fn get_balance(&self) -> u32 {
+        self.balance
+    }
+
+    pub fn get_type(&self) -> u32 {
+        self.item_type
+    }
+
+}
+
+struct Inventory {
+    stacks: Vec<Stack>
+}
+
+impl Inventory {
+    pub fn new(stacks: &[Stack]) -> Self {
+        Self {
+            stacks
+        }
     }
 }
 
@@ -238,15 +390,35 @@ struct InventoryManager {
 }
 
 impl InventoryManager {
+
+    pub async fn new(pool: PgPool) -> Self {
+        Self { pool }
+    }
+
     pub async fn get_inventory(&self, account_id: &str) -> Result<Inventory, sqlx::Error> {
-        let stacks = sqlx::query_as!(Stack,
-            r#"SELECT stack_uuid, item_type, balance FROM latest
-            WHERE account_id = $1 AND balance > 0;
-            "#,
+
+        let mut stacks: Vec<Stack> = Vec::new();
+
+        let latest_keys = sqlx::query!(
+            r#"SELECT latest_keys FROM inventories
+            WHERE account_id = $1;"#,
             account_id)
-            .fetch_all(&mut self.pool)
+            .fetch_one(&mut self.pool)
             .await?;
-        let inventory = Inventory::new(&stacks[..]);
+        
+        for key in latest_keys {
+            let stack = sqlx::query!(
+                r#"SELECT stack_uuid, balance, item_type FROM latest
+                WHERE key = $1;"#,
+                key)
+                .fetch_one(&mut self.pool)
+                .await?;
+
+            stacks.push(stack);
+        }
+
+
+        let inventory = Inventory::new(&stacks);
         Ok(inventory)
     }
 }
--- migrations/20250731014856_init.sql
+++ migrations/20250731014856_init.sql
@@ -32,17 +32,24 @@ EXCLUDE USING hash (
 -- To track prunable stacks
 CREATE TABLE stacks (
     stack_uuid BYTEA NOT NULL,
-    account_ids TEXT[] NOT NULL DEFAULT '{}'
+    latest_keys BYTEA[] NOT NULL DEFAULT '{}',
+    ledger_entries BIGINT[] NOT NULL DEFAULT '{}',
 );
 
+-- If this was needed but I think it isn't, given the application layer logic, and skipping it reduces hash creation (insertion overhead)
+-- ALTER TABLE stacks ADD CONSTRAINT exc_stacks_stack_uuid
+-- EXCLUDE USING hash (
+--     stack_uuid WITH =
+-- );
+
 -- B-tree partial index to track the prunable stacks, keeps o(1) on non-empty stack insertion
-CREATE INDEX idx_stacks_empty_accounts ON stacks (stack_uuid)
-WHERE account_ids = '{}';
+CREATE INDEX idx_stacks_empty_accounts ON stacks USING btree (stack_uuid)
+WHERE latest_keys = '{}';
 
 -- For player inventories
 CREATE TABLE inventories (
     account_id TEXT NOT NULL,
-    stack_uuids BYTEA[] NOT NULL DEFAULT '{}'
+    latest_keys BYTEA[] NOT NULL DEFAULT '{}'
 );
 
 CREATE INDEX idx_inventories_account_id_hash ON inventories USING hash (account_id);
@@ -54,6 +61,7 @@ EXCLUDE USING hash (
 
 -- Ledger table for transaction history
 CREATE TABLE ledger (
+    key BIGINT GENERATED ALWAYS AS IDENTITY,
     account_id TEXT NOT NULL,
     stack_uuid BYTEA NOT NULL,
     sequence_number INTEGER NOT NULL CHECK (sequence_number >= 0),
@@ -63,6 +71,9 @@ CREATE TABLE ledger (
     CONSTRAINT no_sub_zero_balance CHECK (balance >= 0)
 );
 
+-- To prune
+CREATE INDEX idx_ledger_key ON ledger USING hash (key);
+
 -- Exclusion constraint for composite uniqueness
 ALTER TABLE ledger ADD CONSTRAINT exc_ledger_composite
 EXCLUDE USING hash (
@@ -70,3 +81,4 @@ EXCLUDE USING hash (
     stack_uuid WITH =,
     sequence_number WITH =
 );
+-- crear una BYTEA key y usar solo esa para el exclusion constraint, ya que postgres no puede hacer exclusion constraits de multiples columnas
